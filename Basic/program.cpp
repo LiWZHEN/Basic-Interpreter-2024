7ecd4828b0691a47a705a8d641c4ded6271c1d66
lwz <livandan@sjtu.edu.cn>
2024-12-02 10:03:59 +0800

    Update program.cpp

--- Basic/program.cpp
+++ Basic/program.cpp
@@ -11,7 +11,6 @@
 #include "program.hpp"
 
 
-
 Program::Program() = default;
 
 Program::~Program() = default;
@@ -19,26 +18,52 @@ Program::~Program() = default;
 void Program::clear() {
     // Replace this stub with your own code
     //todo
+    info.clear(); // 清除所有的源代码
+
+    // 清除所有的解析对象，确保删除指针避免内存泄漏
+    for (auto &entry : storage) {
+        delete entry.second;
+    }
+    storage.clear();
+
+    // 清除执行次数
+    executionCount.clear();
 }
 
 void Program::addSourceLine(int lineNumber, const std::string &line) {
     // Replace this stub with your own code
     //todo
+    deleteParsedStatement(lineNumber);
+    info[lineNumber] = line;
+    executionCount[lineNumber] = 0;
 }
 
 void Program::removeSourceLine(int lineNumber) {
     // Replace this stub with your own code
     //todo
+    info.erase(lineNumber);
+    deleteParsedStatement(lineNumber);
+    executionCount.erase(lineNumber);
 }
 
 std::string Program::getSourceLine(int lineNumber) {
     // Replace this stub with your own code
     //todo
+    if (info.find(lineNumber) != info.end()) {
+        return info[lineNumber];
+    }
+    return "";
 }
 
 void Program::setParsedStatement(int lineNumber, Statement *stmt) {
     // Replace this stub with your own code
     //todo
+    if (storage.find(lineNumber) != storage.end()) { // 如果之前存在 Statement，需要删除以避免内存泄漏
+        delete storage[lineNumber];
+    }
+
+    // 更新 storage 中该行的解析对象
+    storage[lineNumber] = stmt;
 }
 
 //void Program::removeSourceLine(int lineNumber) {
@@ -46,19 +71,43 @@ void Program::setParsedStatement(int lineNumber, Statement *stmt) {
 Statement *Program::getParsedStatement(int lineNumber) {
    // Replace this stub with your own code
    //todo
+    if (storage.find(lineNumber) != storage.end()) { // 返回行号对应的解析后对象
+        return storage[lineNumber];
+    }
+    return nullptr; // 找不到就返回空指针
 }
 
 int Program::getFirstLineNumber() {
-    // Replace this stub with your own code
-    //todo
+    if (info.empty()) {
+        return -1;
+    }
+    int minLine = INT_MAX;
+    for (const auto &p : info) {
+        minLine = std::min(minLine, p.first);
+    }
+    return minLine;
 }
 
 int Program::getNextLineNumber(int lineNumber) {
-    // Replace this stub with your own code
-    //todo
+    int minLine = INT_MAX;
+    bool found = false;
+    for (const auto &p : info) {
+        if (p.first > lineNumber) {
+            minLine = std::min(minLine, p.first);
+            found = true;
+        }
+    }
+    return found ? minLine : -1; // 有下一行返回下一行行号，没有就返回-1
 }
 
 //more func to add
 //todo
 
+void Program::AddTimes(int lineNumber) {
+    ++executionCount[lineNumber];
+    if (executionCount[lineNumber] >= 4294967295) {
+        error("SYNTAX ERROR");
+        exit(1);
+    }
+}
 
