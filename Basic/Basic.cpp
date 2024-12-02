1a1993f0e60c6beacf214afd40bf892ca50bb094
lwz <livandan@sjtu.edu.cn>
2024-12-02 09:57:29 +0800

    Update Basic.cpp

--- Basic/Basic.cpp
+++ Basic/Basic.cpp
@@ -18,7 +18,8 @@
 /* Function prototypes */
 
 void processLine(std::string line, Program &program, EvalState &state);
-
+void runProgram(Program &program, EvalState &state);
+void listProgram(Program &program);
 /* Main program */
 
 int main() {
@@ -57,6 +58,161 @@ void processLine(std::string line, Program &program, EvalState &state) {
     scanner.scanNumbers();
     scanner.setInput(line);
 
-    //todo
+    if (scanner.hasMoreTokens()) {
+        std::string token = scanner.nextToken();
+
+        if (isdigit(token[0])) { // 行号开头
+            int lineNumber = stringToInteger(token);
+            if (!scanner.hasMoreTokens()) { // 如果行号后面没有更多内容，表示是删除该行
+                program.removeSourceLine(lineNumber);
+            } else { // 有内容
+                std::string stmtToken = scanner.nextToken(); // 辨别类型，以便根据不同类型创建 Statement 对象
+                Statement *stmt = nullptr;
+
+                if (stmtToken == "LET") {
+                    std::string var = scanner.nextToken();
+                    if (scanner.nextToken() != "=") {
+                        error("SYNTAX ERROR");
+                    }
+                    Expression *exp = parseExp(scanner);
+                    stmt = new LetStatement(var, exp);
+                } else if (stmtToken == "PRINT") {
+                    Expression *exp = parseExp(scanner);
+                    stmt = new PrintStatement(exp);
+                } else if (stmtToken == "INPUT") {
+                    if (!scanner.hasMoreTokens()) {
+                        error("SYNTAX ERROR");
+                    }
+                    const std::string var = scanner.nextToken();
+                    stmt = new InputStatement(var);
+                } else if (stmtToken == "REM") {
+                    std::string commentText = scanner.getRemainingInput(); // 获取 REM 后的所有内容
+                    stmt = new RemStatement(commentText);  // 生成 REM 语句
+                } else if (stmtToken == "GOTO") {
+                    if (!scanner.hasMoreTokens()) {
+                        error("SYNTAX ERROR");
+                    } // 缺目标行
+                    int targetLine = stringToInteger(scanner.nextToken());
+                    if (program.getSourceLine(targetLine) == "") {
+                        error("LINE NUMBER ERROR");
+                    } // 目标行错误
+                    if (scanner.hasMoreTokens()) {
+                        error("SYNTAX ERROR");
+                    } // 多了不该有的
+                    stmt = new GotoStatement(targetLine);
+                } else if (stmtToken == "IF") {
+                    if (!scanner.hasMoreTokens()) {
+                        error("SYNTAX ERROR");
+                    } // 缺表达式
+                    Expression *lhs = readE(scanner);
+                    if (!scanner.hasMoreTokens()) {
+                        error("SYNTAX ERROR");
+                    } // 缺符号
+                    std::string op = scanner.nextToken();
+                    if (op != "<" && op != "=" && op != ">") {
+                        error("SYNTAX ERROR");
+                    } // 符号不对
+                    if (!scanner.hasMoreTokens()) {
+                        error("SYNTAX ERROR");
+                    } // 符号后缺数
+                    Expression *rhs = readE(scanner);
+                    if (!scanner.hasMoreTokens()) {
+                        error("SYNTAX ERROR");
+                    } // 缺跳转
+                    scanner.verifyToken("THEN");
+                    if (!scanner.hasMoreTokens()) {
+                        error("SYNTAX ERROR");
+                    } // 缺目标行
+                    int targetLine = stringToInteger(scanner.nextToken());
+                    if (program.getSourceLine(targetLine) == "") {
+                        error("LINE NUMBER ERROR");
+                    } // 目标行错误
+                    if (scanner.hasMoreTokens()) {
+                        error("SYNTAX ERROR");
+                    } // 多了不该有的
+                    stmt = new IfStatement(lhs, op, rhs, targetLine);
+                } else if (stmtToken == "EXIT") {
+                    stmt = new EndStatement();
+                } else {
+                    error("SYNTAX ERROR");
+                }
+
+                if (stmt != nullptr) { // stmt有效，存它
+                    program.addSourceLine(lineNumber, line);
+                    program.setParsedStatement(lineNumber, stmt);
+                }
+            }
+        } else {
+            // 处理命令的情况（即没有行号的命令）
+            Statement *stmt = nullptr;
+
+            if (token == "RUN") {
+                runProgram(program, state);
+            } else if (token == "LIST") {
+                listProgram(program);
+            } else if (token == "CLEAR") {
+                program.clear();
+                state.Clear();
+            } else if (token == "QUIT") {
+                exit(0);
+            } else if (token == "LET") {
+                const std::string var = scanner.nextToken();
+                if (scanner.nextToken() != "=") {
+                    error("SYNTAX ERROR");
+                }
+                Expression *exp = parseExp(scanner);
+                stmt = new LetStatement(var, exp);
+            } else if (token == "PRINT") {
+                Expression *exp = parseExp(scanner);
+                stmt = new PrintStatement(exp);
+            } else if (token == "INPUT") {
+                std::string var = scanner.nextToken();
+                if (scanner.hasMoreTokens()) {
+                    error("SYNTAX ERROR");
+                }
+                stmt = new InputStatement(var);
+            } else {
+                error("SYNTAX ERROR");
+            }
+
+            // 立即执行命令
+            if (stmt != nullptr) {
+                stmt->execute(state, program);
+                delete stmt; // 删除立即执行的语句，避免内存泄漏
+            }
+        }
+    }
 }
 
+
+void runProgram(Program &program, EvalState &state) {
+    int lineNumber = program.getFirstLineNumber();
+    while (lineNumber != -1) {
+        Statement *stmt = program.getParsedStatement(lineNumber);
+        if (stmt != nullptr) {
+            program.AddTimes(lineNumber);
+            if (const auto *gotoStmt = dynamic_cast<GotoStatement*>(stmt)) { // stmt是GotoStatement类型的
+                lineNumber = gotoStmt->getTargetLine();
+            } else if (const auto *ifStmt = dynamic_cast<IfStatement*>(stmt)) { // stmt是IfStatement类型的
+                if (ifStmt->isConditionTrue(state)) {
+                    lineNumber = ifStmt->getTargetLine();
+                } else {
+                    lineNumber = program.getNextLineNumber(lineNumber);
+                }
+            } else if (dynamic_cast<EndStatement*>(stmt)) { // stmt是EndStatement类型的
+                lineNumber = -1;
+            } else { // 其他，正常转移
+                stmt->execute(state, program);
+                lineNumber = program.getNextLineNumber(lineNumber);
+            }
+        }
+    }
+}
+
+void listProgram(Program &program) {
+    int lineNumber = program.getFirstLineNumber();
+    while (lineNumber != -1) {
+        std::cout << lineNumber << " " << program.getSourceLine(lineNumber) << std::endl;
+        lineNumber = program.getNextLineNumber(lineNumber);
+    }
+}
